5ca9a0f361cbf729b2427fab9168f302116c9f1b
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-04 17:13:27 +0000

    84124d55bb841c0bc66fda48a47d3106917574aa
    Sync to source repo @84124d55bb841c0bc66fda48a47d3106917574aa

--- js/responsive.jqueryui.min.mjs
+++ js/responsive.jqueryui.min.mjs
@@ -1,1 +1,4 @@
+/*! jQuery UI integration for DataTables' Responsive
+ * © SpryMedia Ltd - datatables.net/license
+ */
 import $ from"jquery";import DataTable from"datatables.net-ju";import DataTable from"datatables.net-responsive";var _display=DataTable.Responsive.display,_original=_display.modal;_display.modal=function(i){return function(a,e,t){$.fn.dialog?e||$("<div/>").append(t()).appendTo("body").dialog($.extend(!0,{title:i&&i.header?i.header(a):"",width:500},i.dialog)):_original(a,e,t)}};export default DataTable;

a5361e6f22518b1dcca43a7d6b5bf53f4c2dd82d
Allan Jardine <allan@datatables.net>
2026-05-07 15:36:57 +0000

    Updating package for DataTables 3

--- js/responsive.jqueryui.js
+++ js/responsive.jqueryui.js
@@ -1,7 +1,54 @@
-/*! jQuery UI integration for DataTables' Responsive
- * © SpryMedia Ltd - datatables.net/license
+/*! Responsive jQuery UI styling 4.0.0-beta.1 for DataTables
+ * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 
+(function(factory){
+	if (typeof define === 'function' && define.amd) {
+		// AMD
+		define(['datatables.net-jqui', 'datatables.net-responsive'], function (dt) {
+			return factory(window, document, dt);
+		});
+	}
+	else if (typeof exports === 'object') {
+		// CommonJS
+		var cjsRequires = function (root) {
+			if (! root.DataTable) {
+				require('datatables.net-jqui')(root);
+			}
+
+			if (! window.DataTable.Responsive) {
+				require('datatables.net-responsive')(root);
+			}
+		};
+
+		if (typeof window === 'undefined') {
+			module.exports = function (root) {
+				if (! root) {
+					// CommonJS environments without a window global must pass a
+					// root. This will give an error otherwise
+					root = window;
+				}
+
+				cjsRequires(root);
+				return factory(root, root.document, root.DataTable);
+			};
+		}
+		else {
+			cjsRequires(window);
+			module.exports = factory(window, window.document, window.DataTable);
+		}
+	}
+	else {
+		// Browser
+		factory(window, document, window.DataTable);
+	}
+}(function(window, document, DataTable) {
+'use strict';
+
+
+
+// Note that jQuery UI's JS depends upon jQuery, so we use it here
+var $ = DataTable.use('jq');
 var _display = DataTable.Responsive.display;
 var _original = _display.modal;
 
@@ -41,3 +88,7 @@ _display.modal = function (options) {
 		}
 	};
 };
+
+
+return DataTable;
+}));
